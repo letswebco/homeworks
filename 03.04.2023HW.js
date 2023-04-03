d99c44ba5e77232fb5b261e7728dc8190ee57194
Letsweb.co <whooleets@gmail.com>
2023-04-03 15:56:24 +0400

    update=> Code

---
+++ 03.04.2023HW.js
@@ -0,0 +1,21 @@
+console.log("hi")
+
+
+
+function guessTheNumber(number) {
+    let promtNumber = prompt("sheiyvanet gamosacnobi ricxvi");
+    if (promtNumber==number){
+        prompt("you won")
+    }
+    else if (promtNumber>number) {
+        prompt("sheni ricxvi metia")
+    }
+    else if (promtNumber<number) {
+        prompt("sheni ricxvi naklebia")
+    }
+}
+
+
+
+
+guessTheNumber(22)
